d94ecea6bb32b64024198b275b7dbd1d022674b1
Mavericks <mavericks@maverickss-macbook-pro.local>
2015-11-23 13:32:14 +0530

    Initial Commit

---
+++ CropAnImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CropAnImage
+//
+//  Created by Mavericks on 23/11/15.
+//  Copyright © 2015 Mavericks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
